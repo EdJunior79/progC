a1d03764f3149410690170d0418ceb00f358fb9e
EdJunior79 <edcaiaffa@gmail.com>
2025-04-09 14:40:15 -0300

    Update lista3.c

---
+++ lista3.c
@@ -0,0 +1,27 @@
+void Q1() {
+	int v[4]; int par = 0;
+	printf("Preenchendo o vetor:\n");
+	printf("V[0]= \n");
+	scanf("&d", &v[0]);
+	printf("V[1]= \n");
+	scanf("&d", &v[1]);
+	printf("V[2]= \n");
+	scanf("&d", &v[2]);
+	printf("V[3]= \n");
+	scanf("&d", &v[0]);
+	printf("V[4]= \n");
+	scanf("&d", &v[4]);
+	if v[x] % 2 == 0{
+		par++
+	}return par
+
+}
+
+
+
+
+
+
+main() {
+	return 0;
+}
