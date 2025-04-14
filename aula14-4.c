db22080daa9c2906c252ac709afdb05fb95804f5
EdJunior79 <edcaiaffa@gmail.com>
2025-04-14 14:06:11 -0300

    Update aula14-4.c

--- aula14-4.c
+++ aula14-4.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void captura_valores(int n, float* v ) {
+	printf("Entre com os valores a serem armazenados no vetor:\n");
+	for (i=0,i<n,i+=1) {
+		scanf_s("%f", &v[i]);
+	}
+}
+
+float calcula_somatorio(int n, float* v) {
+	float soma = 0.0f;
+	for (int i = 0, i < n; i += 1) {
+		soma += v[i];
+	}
+	return soma
+}
+
+
+
 int main() {
 	int n;
 	printf("Entre com a quantidade de valores a ser armazenados no vetor: ");
